533b7198e79251b969a5bbfa3866e6aa59271557
MasterLenoX <lenoxemperado@gmail.com>
2024-10-22 15:44:08 +0800

    2nd commit - setting up laravel url routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('author')->name('author.')->group(function(){
+    Route::middleware(['guest:web'])->group(function(){
+        Route::view('/login','backend.pages.auth.login')->name('login');
+        Route::view('/forgot-password','backend.pages.auth.forgot')->name('forgot-password');
+    });
+
+    Route::middleware([])->group(function(){
+        Route::get('/home',[AuthorController::class,'index'])->name('home');
+    });
 });
